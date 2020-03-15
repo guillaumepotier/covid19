fa618a159d6105294fde89f9e29e3f407dc628c9
Guillaume Potier <cobalt2760@gmail.com>
2020-03-15 23:21:12 +0100

    Initial dist subtree commit

---
+++ build/precache-manifest.6ab4ebcf4873c70b7f779191cb75fe37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39405ef75d73b8a6f970fcc60b1cd22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab00af0f1db5f415cf88",
+    "url": "/static/css/main.ec2375f5.chunk.css"
+  },
+  {
+    "revision": "64434a3a42c28d54faae",
+    "url": "/static/js/2.87048c96.chunk.js"
+  },
+  {
+    "revision": "6d3c4316041b5905aa31b3ff080c57e2",
+    "url": "/static/js/2.87048c96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab00af0f1db5f415cf88",
+    "url": "/static/js/main.6e5a9f60.chunk.js"
+  },
+  {
+    "revision": "dd05f9b019343fa06e10",
+    "url": "/static/js/runtime-main.51344e82.js"
+  }
+]);
